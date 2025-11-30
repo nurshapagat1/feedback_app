11c0ffaecb0c5b6e8c8e4460a61837c9f65bd1df
nurshapagat1 <nurshapagat879@gmail.com>
2025-11-30 17:53:02 +0500

    host12778

--- FeedBack/settings.py
+++ FeedBack/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")=="True"
 
-ALLOWED_HOSTS = ["localhost","http://127.0.0.1:8000/"]
+ALLOWED_HOSTS = ["localhost","127.0.0.1","*"]
 
 
 # Application definition
