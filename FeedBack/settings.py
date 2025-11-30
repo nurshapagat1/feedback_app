69558b50d2bd3330d7b771b563eeabaca551ce73
nurshapagat1 <nurshapagat879@gmail.com>
2025-11-30 18:02:27 +0500

    host1277899

--- FeedBack/settings.py
+++ FeedBack/settings.py
@@ -127,3 +127,6 @@ STATIC_ROOT=BASE_DIR/"staticfiles"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = [
+    'https://feedback-app-m6hp.onrender.com',
+]
